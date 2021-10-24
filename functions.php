feb4139694333912f23d8e86e1a134bfb082dc35
Rizky Anfasa Farras Mada <rizkyanfasafm@gmail.com>
2021-10-24 19:13:54 +0700

    add post

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@ function followrizky_theme_support(){
     // Add dyanmic title tag support
     add_theme_support('title-tag');
     add_theme_support('custom-logo');
+    add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'followrizky_theme_support');
